bd6262712d826b771c8120d25b74e4ba1f48db36
Michael Rubenstein <mrubenstein6192@gmail.com>
2020-04-07 12:40:26 +1000

    adding pics

--- logic.js
+++ logic.js
@@ -47,17 +47,95 @@ $(document).ready(function() {
     $("#main-container").show();
     $("#poster1").on("click", function() {
       $("#user-message").html("You clicked on Poster 1");
-      $("#user-images").html("A Pic will Show Here")
+      var poster1Image = new Image(250,250);
+      poster1Image.classList.add("thisPic");
+      poster1Image.src = ("./images/poster1.png");
+      $("#user-images").html(poster1Image);
+     
     })
-    $("#object2").on("click", function() {
+    $("#table1").on("click", function() {
       $("#user-message").html("You clicked on Object 2");
-      $("#user-images").html("A Pic will Show Here")
+      var tableImage = new Image(250,250);
+      tableImage.classList.add("thisPic");
+      tableImage.src = ("./images/table.png");
+      $("#user-images").html(tableImage);
     })
-    $("#object3").on("click", function() {
+    $("#circleTable").on("click", function() {
       $("#user-message").html("You clicked on Object 3");
       $("#user-images").html("A Pic will Show Here")
     })
-
+    $("#object4").on("click", function() {
+      $("#user-message").html("You clicked on Object 4");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object5").on("click", function() {
+      $("#user-message").html("You clicked on Object 5");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object6").on("click", function() {
+      $("#user-message").html("You clicked on Object 6");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object7").on("click", function() {
+      $("#user-message").html("You clicked on Object 7");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object8").on("click", function() {
+      $("#user-message").html("You clicked on Object 8");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object9").on("click", function() {
+      $("#user-message").html("You clicked on Object 9");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object10").on("click", function() {
+      $("#user-message").html("You clicked on Object 10");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object11").on("click", function() {
+      $("#user-message").html("You clicked on Object 11");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object12").on("click", function() {
+      $("#user-message").html("You clicked on Object 12");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object13").on("click", function() {
+      $("#user-message").html("You clicked on Object 13");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#door").on("click", function() {
+      $("#user-message").html("You clicked on the Door! <br/>Do you know the code to escape?!");
+      var doorImage = new Image(250,250);
+      doorImage.classList.add("thisPic");
+      doorImage.src = ("./images/doorCode.jpg");
+      $("#user-images").html(doorImage);
+    })
+    $("#object15").on("click", function() {
+      $("#user-message").html("You clicked on Object 15");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object16").on("click", function() {
+      $("#user-message").html("You clicked on Object 16");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object17").on("click", function() {
+      $("#user-message").html("You clicked on Object 17");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object18").on("click", function() {
+      $("#user-message").html("You clicked on Object 18");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object19").on("click", function() {
+      $("#user-message").html("You clicked on Object 19");
+      $("#user-images").html("A Pic will Show Here")
+    })
+    $("#object20").on("click", function() {
+      $("#user-message").html("You clicked on Object 20");
+      $("#user-images").html("A Pic will Show Here")
+    })
+   
 
 
     $("#submitNote").on("click", function() {
